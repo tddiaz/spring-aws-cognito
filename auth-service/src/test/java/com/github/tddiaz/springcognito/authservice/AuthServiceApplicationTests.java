db8f6be5de3a1cb10aa0f16df30bfcd0ec0b09e9
Tristan Diaz <tristan.diaz@TristanEkarMBP.net.sahm.ae>
2020-09-15 16:29:53 +0400

    first commit

---
+++ auth-service/src/test/java/com/github/tddiaz/springcognito/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tddiaz.springcognito.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
